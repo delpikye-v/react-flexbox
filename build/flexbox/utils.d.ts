3710d60e5484cc9c7d7c95390ff2b6c68dfdf626
Delpi (Tung Vu) <tungvuit@gmail.com>
2026-05-28 01:02:16 +0700

    Delete build directory

--- build/flexbox/utils.d.ts
+++
@@ -1,3 +0,0 @@
-import type { IndexableCSS } from './IFlexType';
-export declare const removeUndefined: (style: IndexableCSS) => IndexableCSS;
-export declare const notifyOverlapping: (propName: string, ...props: (boolean | undefined)[]) => void;
